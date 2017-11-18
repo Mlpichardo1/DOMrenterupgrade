9628b8cf52a92aff8c0791900ec0c84e281eded4
Manuel Pichardo <mlpichardo1@gmail.com>
2017-11-18 02:31:48 +0000

    Car Rental submit

--- objects1.js
+++ objects1.js
@@ -1,96 +1,53 @@
-function rent() {
 
-	var errormessage = "";
-	
-	if (document.carForm.fullName.value == "") {
-	errormessage += "Enter your Name \n";
-}
-
-	if (document.carForm.fullName.value == "") {
-	errormessage += "Enter your Name \n";
-	}
-
-	if (errormessage != "") {
-	alert(errormessage);
-	return false;
-	}	
-}
-
-
+	var rentalName = {
+	 nameOfRenter: "Manny Pichardo",
+	 carType: ["Economy", "Midsize"],
+	};
 
    var cars = { 
 	 name: ["Economy", "Midsize"],
-	 economyCars: 1,
+	 economyCars: 3,
 	 economyCarsBooked: 0,
-	 midsizeCars: 1,
+	 midsizeCars: 5,
 	 midsizeCarsBooked: 0,
+	 econPrice: 99,
+	 midPrice: 149,
+   }
 	 
-	  ecarFunction: function() {
-	   return this.economyCars - this.economyCarsBooked;
-	  },
+	  //ecarFunction: function() {
+	  // return this.economyCars - this.economyCarsBooked;
+	  //},
 	  
-	  midcarFunction: function() {
-	   return this.midsizeCars - this.midsizeCarsBooked;
-	  },
+	  //midcarFunction: function() {
+	  // return this.midsizeCars - this.midsizeCarsBooked;
+	  //},
 	  
-	  bookEcon: function() {
-	      return this.economyCarsBooked++;
-	  },
+	  //bookEcon: function() {
+	  //    return this.economyCarsBooked++;
+	  //},
 	  
-	  bookMid: function() {
-	      return this.midsizeCarsBooked++;
-	  }
-	  };
+	  //bookMid: function() {
+	  //    return this.midsizeCarsBooked++;
+	  //}
+	  //};
 	    
-	function availFunction() {
-	document.getElementById("ecars").innerHTML = cars.name[0];
-	document.getElementById("midcars").innerHTML = cars.name[1];
-	document.getElementById("ecarsAvail").innerHTML = cars.ecarFunction();
-	document.getElementById("midcarsAvail").innerHTML = cars.midcarFunction();
-	}
+	// function availFunction() {
+	// document.getElementById("ecars").innerHTML = cars.name[0];
+	// document.getElementById("midcars").innerHTML = cars.name[1];
+	// document.getElementById("ecarsAvail").innerHTML = cars.ecarFunction();
+	// document.getElementById("midcarsAvail").innerHTML = cars.midcarFunction();
+	// }
 	
-	function rentCar() {
-	cars.bookEcon();
-	document.getElementById("ecarsAvail").innerHTML = cars.ecarFunction();
-	}
+	// function rentCar() {
+	// cars.bookEcon();
+	// document.getElementById("ecarsAvail").innerHTML = cars.ecarFunction();
+	// }
 
-    function rentMidcar(){
-    cars.bookMid();
-	document.getElementById("midcarsAvail").innerHTML = cars.midcarFunction();    
-    }
+ //   function rentMidcar(){
+ //   cars.bookMid();
+	// document.getElementById("midcarsAvail").innerHTML = cars.midcarFunction();    
+ //   }
+    
+
+
 	
-// var carRental = {
-//     rental: [
-//         {
-//             name:
-//             type:
-//         }
-//         ]
-// },
-//         {
-//             name:
-//             type:
-//         }
-        
-// };
-
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
